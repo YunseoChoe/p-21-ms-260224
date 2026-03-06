d030ff9ee04d1365dcd4ca138ce31c82b4b35dcd
choiyunseo <choeyunseo4060@naver.com>
2026-03-06 22:45:58 +0900

    Feat: 2-16

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,13 +2,14 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import com.mysite.sbb.answer.AnswerForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor // final이 붙은 속성을 포함하는 생성자를 자동으로 만들어줌.
 @Controller
@@ -27,9 +28,26 @@ public class QuestionController {
 
     // 일부 목록
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    // 질문하기 페이지 가져오기.
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    // 질문 등록하기.
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
     }
 }
